fba68d6a852c9cba472ab25e8f466994fb6fae14
Leandro Schwab Dias Carneiro <schwab_@hotmail.com>
2018-10-14 23:40:36 -0300

    Update remedio
    Servidor agora envia informação de remedios para os clientes

--- src/main/java/ThreadCliente.java
+++ src/main/java/ThreadCliente.java
@@ -12,72 +12,114 @@ import _functions.ConnMySQL;
 public class ThreadCliente extends Thread {
 	private Socket clientSocket;
 
-	  public ThreadCliente(Socket clientSocket) {
-	    this.clientSocket = clientSocket; 
-	  }
-	   
-	public void run(){
+	public ThreadCliente(Socket clientSocket) {
+		this.clientSocket = clientSocket;
+	}
+
+	public void run() {
 		try {
-	        Scanner din = new Scanner(clientSocket.getInputStream());
-	        String msgRecebida = din.nextLine();
-	        System.out.println("Servidor: recebeu o valor " + msgRecebida);
-	        String aMsgRec[]= msgRecebida.split("-;-");
-	        System.out.println("Servidor: aMsgRec[0] " + aMsgRec[0]);
-	        
-	        
-	        if(aMsgRec[0].equals("Login")) {	
-	        	String msgEnviar = opLogin(aMsgRec[1],aMsgRec[2].toUpperCase());
-			    PrintWriter dout = new PrintWriter(clientSocket.getOutputStream(), true);
-		        dout.println(msgEnviar);
+			Scanner din = new Scanner(clientSocket.getInputStream());
+			String msgRecebida = din.nextLine();
+			System.out.println("Servidor: recebeu o valor " + msgRecebida);
+			String aMsgRec[] = msgRecebida.split("-;-");
+			System.out.println("Servidor: aMsgRec[0] " + aMsgRec[0]);
+
+			if (aMsgRec[0].equals("Login")) {
+				String msgEnviar = opLogin(aMsgRec[1], aMsgRec[2].toUpperCase());
+				PrintWriter dout = new PrintWriter(clientSocket.getOutputStream(), true);
+				dout.println(msgEnviar);
 				System.out.println("Servidor: enviou o valor " + msgEnviar);
-	        }
-	        
-	        
+			} else if (aMsgRec[0].equals("Remedio")) {
+				String msgEnviar = opRemedio(aMsgRec[1]);
+				PrintWriter dout = new PrintWriter(clientSocket.getOutputStream(), true);
+				dout.println(msgEnviar);
+				System.out.println("Servidor: enviou o valor " + msgEnviar);
+			}
 
 			din.close();
 
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
-	private String opLogin(String id,String senha) {
-		String msgEnviar;
+	private String opRemedio(String id) {
+		String msgEnviar = new String();
 		try {
 			System.out.println("Servidor: opLogin...");
-			
+
 			Connection conn = ConnMySQL.openConnMySQL();
-			
-			String sql = "SELECT `nome-paciente`, `password` FROM `receitas` WHERE `id-receita`=" + id + "";
-	
+
+			String sql = "SELECT `Nome Remedio`, `cor`,`6:00`,`7:00`,`12:00`,`19:00`,`21:00` FROM `horarios` WHERE `id-receita`="
+					+ id + "";
+
 			try (PreparedStatement stmt = conn.prepareStatement(sql)) {
 				try (ResultSet rs = stmt.executeQuery()) {
-					if(rs.next()) {
+					if (rs.next()) {
+						System.out.println("=========================");
+						System.out.println("msgEnviar = " + msgEnviar);
+						System.out.println("=========================");
+						msgEnviar = rs.getString("Nome Remedio") + "-;-" + rs.getString("cor") + "-;-"
+								+ rs.getInt("6:00") + "-;-" + rs.getInt("7:00") + "-;-" + rs.getInt("12:00") + "-;-"
+								+ rs.getInt("19:00") + "-;-" + rs.getInt("21:00");
+						while (rs.next()) {
+							System.out.println("=========================");
+							System.out.println("msgEnviar = " + msgEnviar);
+							System.out.println("=========================");
+							msgEnviar = msgEnviar + "-:-" + rs.getString("Nome Remedio") + "-;-" + rs.getString("cor")
+									+ "-;-" + rs.getInt("6:00") + "-;-" + rs.getInt("7:00") + "-;-" + rs.getInt("12:00")
+									+ "-;-" + rs.getInt("19:00") + "-;-" + rs.getInt("21:00");
+
+						}
+
+					}
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("=========================");
+		System.out.println("msgEnviar = " + msgEnviar);
+		System.out.println("=========================");
+		return msgEnviar;
+	}
+
+	private String opLogin(String id, String senha) {
+		String msgEnviar = new String();
+		try {
+			System.out.println("Servidor: opLogin...");
+
+			Connection conn = ConnMySQL.openConnMySQL();
+
+			String sql = "SELECT `nome-paciente`, `password` FROM `receitas` WHERE `id-receita`=" + id + "";
+
+			try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+				try (ResultSet rs = stmt.executeQuery()) {
+					if (rs.next()) {
 						String senhaDB = rs.getString("password");
 						if (senhaDB.equals(senha)) {
 							String nomePaciente = rs.getString("nome-paciente");
 							System.out.println("=========================");
 							System.out.println("Nome = " + nomePaciente);
 							System.out.println("=========================");
-							msgEnviar = "login" + "-;-SucessoLogin";
-							
-							
-							
-						}else {
+							msgEnviar = "login" + "-;-SucessoLogin-;-"+nomePaciente;
+
+						} else {
 							System.out.println("=========================");
-							System.out.println("Login invalido "+ senha +" "+ senhaDB);
+							System.out.println("Login invalido " + senha + " " + senhaDB);
 							System.out.println("=========================");
 							msgEnviar = "login" + "-;-FalhaLogin-;-Senha incorreta";
 						}
-					}else {
+					} else {
 						System.out.println("=========================");
 						System.out.println("Login invalido não encontrado");
 						System.out.println("=========================");
 						msgEnviar = "login" + "-;-FalhaLogin-;-Senha incorreta";
 					}
-					
+
 				}
 			}
 		} catch (Exception e) {
